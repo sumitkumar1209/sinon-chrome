9d42851f5dff68c6f71de32a8033f6ecbcee38d5
Sumit Kumar <sumitkumar1209@gmail.com>
2015-11-21 00:36:58 +0530

    chrome api framework

---
+++ src/chrome-api.js
@@ -0,0 +1,25 @@
+/**
+ * Created by sumit on 20/11/15.
+ * tabs.executeScript implementation by eval function, if a code is provided, otherwise call a function from the file name
+ */
+(function () {
+
+  var chromeApi = {
+    executeScript: function (objectDetail) {
+      if (objectDetail.hasOwnProperty("code")) {
+        eval(objectDetail["code"])
+      }
+      else if (objectDetail.hasOwnProperty("file") && objectDetail.hasOwnProperty("init_method")) {
+        objectDetail["init_method"].call()
+      }
+    }
+  };
+  // exports
+  if (typeof exports === 'object') {
+    module.exports = chromeApi;
+  } else {
+    window.chromeApi = chromeApi;
+  }
+}());
+
+
